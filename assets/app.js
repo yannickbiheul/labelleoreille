72035cd5a26db48a83120d5b0d04eaee40f5c24c
Yannick B <yannickbiheul@gmail.com>
2021-05-11 20:03:45 +0200

    Section présentation

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,8 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import './styles/template.scss';
+import './styles/accueil.scss';
 
 // You can specify which plugins you need
 import { Tooltip, Toast, Popover } from 'bootstrap';
